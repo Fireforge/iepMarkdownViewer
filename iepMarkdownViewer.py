2c5469e72074082baec6b4e1311a9ce2bc4a9a42
Fireforge <fireforge124@gmail.com>
2014-03-01 01:58:35 -0500

    Fixed file header

--- iepMarkdownViewer.py
+++ iepMarkdownViewer.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2013, the IEP development team
 #
-# IEP is distributed under the terms of the (new) BSD License.
-# The full license can be found in 'license.txt'.
+# Adapted from the IEP Web Browser Tool
 
 import os
 import time
