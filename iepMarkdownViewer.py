b140a1a8c98644f94d9630ef79d2a8abf7e1924e
fireforge <fireforge124@gmail.com>
2014-03-29 17:22:07 -0400

    Conformed to pep8

--- iepMarkdownViewer.py
+++ iepMarkdownViewer.py
@@ -22,10 +22,11 @@ from iep.tools.iepWebBrowser import WebView
 tool_name = "Markdown Viewer"
 tool_summary = "A live preview of your Markdown in IEP."
 
-accepted_fileext =  [ '.md',
-                      '.markdown',
-                      '.txt']
+accepted_fileext = ['.md',
+                    '.markdown',
+                    '.txt']
 css_link = '<link rel="stylesheet" type="text/css" href="{0}">'
+
 
 def dont_linkify_python(attrs, new=False):
     if not new:  # This is an existing <a> tag, leave it be.
@@ -34,7 +35,7 @@ def dont_linkify_python(attrs, new=False):
     # If the TLD is '.py', make sure it starts with http: or https:
     text = attrs['_text']
     if text.endswith('.py') and \
-        not text.startswith(('www.', 'http:', 'https:')):
+       not text.startswith(('www.', 'http:', 'https:')):
         # This looks like a Python file, not a URL. Don't make a link.
         return None
 
@@ -44,32 +45,35 @@ linkify_configs = {
     'linkifycallbacks': [[dont_linkify_python], '']
 }
 linkify = LinkifyExtension(configs=linkify_configs)
-markdown_extensions = [ 'extra',
-                        'codehilite', 
-                        'sane_lists',
-                        linkify]
+markdown_extensions = ['extra',
+                       'codehilite',
+                       'sane_lists',
+                       linkify]
+
 
 class IepMarkdownViewer(QtGui.QFrame):
     """ The main window, containing browser widget.
     """
-    
+
     def __init__(self, parent):
         QtGui.QFrame.__init__(self, parent)
-        
+
         # Init config
-        toolId =  self.__class__.__name__.lower()
+        toolId = self.__class__.__name__.lower()
         self._config = iep.config.tools[toolId]
-        
+
         # Get style object (for icons)
         style = QtGui.QApplication.style()
-        
+
         # Create web view
         if imported_qtwebkit:
             self._view = QtWebKit.QWebView()
-            self._view.page().setLinkDelegationPolicy(QtWebKit.QWebPage.DelegateAllLinks)
-            self._view.page().linkClicked.connect(self.onLinkClicked)
-            
-            cssurl = os.path.abspath(os.path.dirname(__file__)) + os.sep + "github.css"
+            page = self._view.page()
+            page.setLinkDelegationPolicy(QtWebKit.QWebPage.DelegateAllLinks)
+            page.linkClicked.connect(self.onLinkClicked)
+
+            filedir = os.path.abspath(os.path.dirname(__file__))
+            cssurl = filepath + os.sep + "github.css"
             self._cssurl = QtCore.QUrl.fromLocalFile(cssurl)
         else:
             self._view = WebView(self)
@@ -77,15 +81,14 @@ class IepMarkdownViewer(QtGui.QFrame):
             self._cssurl = None
 #         self._view.settings().setUserStyleSheetUrl(self._cssurl)
 #         print(self._view.settings().userStyleSheetUrl())
-        
+
         # Bind to events
         iep.editors.currentChanged.connect(self.onEditorsCurrentChanged)
         iep.editors.parserDone.connect(self.getEditorContent)
-        
-        
+
         # Create Markdown parser
         self._md = markdown.Markdown(extensions=markdown_extensions)
-        
+
         # Layout
         self._sizer1 = QtGui.QVBoxLayout(self)
         self._sizer2 = QtGui.QHBoxLayout()
@@ -95,23 +98,23 @@ class IepMarkdownViewer(QtGui.QFrame):
         #
         self._sizer1.setSpacing(2)
         self.setLayout(self._sizer1)
-        
+
         # Start
         self.getEditorContent()
         self._view.show()
-    
+
     def onEditorsCurrentChanged(self):
         self.getEditorContent()
-        
+
     def getEditorContent(self):
         # Get editor
         if not iep:
             return
-            
+
         editor = iep.editors.getCurrentEditor()
         if not editor:
             return
-        
+
         if os.path.splitext(editor.filename)[1] in accepted_fileext:
             text = editor.toPlainText()
             html = self._md.convert(text)
@@ -123,4 +126,3 @@ class IepMarkdownViewer(QtGui.QFrame):
 
     def onLinkClicked(self, url):
         webbrowser.open(url.toString())
-            
